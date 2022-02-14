a8f99ad4b4c231f31384d08b6982babf09b983ef
Jan Janssens <jan.janssens@digiti.be>
2022-02-14 11:57:38 +0100

    config update

--- config/crow.php
+++ config/crow.php
@@ -1,30 +1,74 @@
 <?php
 // config for digiti/StatamicCrow
 return [
+    //General
     'include_info_header'  => false,
     'include_info_footer'  => false,
-
     'section_padding'      => '80',
-    'bg_mail'              => '#F9FAFB',
-    'bg_white'             => '#ffffff',
-    'bg_grey'              => '#f2f7fb',
 
+    //Colors
     'primary_color'        => '#e5154d', //to replace #e5154d
     'white_color'          => '#ffffff',
     'heading_color'        => '#516072',
-    'text_color'           => '#8f96a1',     
+    'text_color'           => '#8f96a1', 
+    'bg_mail'              => '#F9FAFB',
+    'bg_white'             => '#ffffff',
+    'bg_grey'              => '#f2f7fb',    
 
+    //Fonts
     'google_font_import'   => 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,700;1,200;1,300;1,400;1,500;1,700&display=swap',
     'font_family'          => '\'Poppins\'',
     'heading_weight'       => '700',
 
+    //Buttons
     'button_shadow'        => '0px 3px 3px 0 rgba(0,0,0,.1)',
     'button_top_margin'    => '30',
     'button_border_width'  => '2',
     'button_border_radius' => '35',
     
+    //Images
     'image_border_radius'  => '10px',
 
+    //Icons
+    'icon_phone_white'     => '/vendor/statamic-crow/images/icons/phone-white.png',
+    'icon_phone_grey'      => '/vendor/statamic-crow/images/icons/phone-grey.png',
+    'icon_email_white'     => '/vendor/statamic-crow/images/icons/email-white.png',
+    'icon_email_grey'      => '/vendor/statamic-crow/images/icons/email-grey.png',
     'icon_check_white'     => '/vendor/statamic-crow/images/icons/check-white.png',
-    'icon_check_grey'      => '/vendor/statamic-crow/images/icons/check-grey.png'
+    'icon_check_grey'      => '/vendor/statamic-crow/images/icons/check-grey.png',
+    'icon_facebook_white'  => '/vendor/statamic-crow/images/icons/facebook-white.png',
+    'icon_facebook_grey'   => '/vendor/statamic-crow/images/icons/facebook-grey.png',
+    'icon_instagram_white' => '/vendor/statamic-crow/images/icons/instagram-white.png',
+    'icon_instagram_grey'  => '/vendor/statamic-crow/images/icons/instagram-grey.png',
+    'icon_twitter_white'   => '/vendor/statamic-crow/images/icons/twitter-white.png',
+    'icon_twitter_grey'    => '/vendor/statamic-crow/images/icons/twitter-grey.png',
+    'icon_linkedin_white'  => '/vendor/statamic-crow/images/icons/linkedin-white.png',
+    'icon_linkedin_grey'   => '/vendor/statamic-crow/images/icons/linkedin-grey.png',
+    'icon_youtube_white'   => '/vendor/statamic-crow/images/icons/youtube-white.png',
+    'icon_youtube_grey'    => '/vendor/statamic-crow/images/icons/youtube-grey.png',
+    'icon_twitch_white'    => '/vendor/statamic-crow/images/icons/twitch-white.png',
+    'icon_twitch_grey'     => '/vendor/statamic-crow/images/icons/twitch-grey.png',
+    'icon_github_white'    => '/vendor/statamic-crow/images/icons/github-white.png',
+    'icon_github_grey'     => '/vendor/statamic-crow/images/icons/github-grey.png',
+
+    //Socials
+    'facebook_link'        => null,
+    'instagram_link'       => null,
+    'twitter_link'         => null,
+    'linkedin_link'        => null,
+    'youtube_link'         => null,
+    'twitch_link'          => null,
+    'github_link'          => null,
+
+    //Header
+    'view_online_label'    => 'View Online',
+
+    //Footer
+    'footer_logo'          => 'https://storage.pardot.com/61642/1613132938bGJWgQOZ/logo_flintgroup.jpg',
+    'footer_logo_width'    => '100',
+    'web_version_label'    => "Web Version",
+    'unsubscribe_label'    => null,
+    'unsubscribe_link'     => "Unsubscribe",
+    'forward_label'        => "Forward",
+    'copyright'            => 'All Rights Reserved',
 ];
